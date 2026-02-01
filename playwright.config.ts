adf0d8a99f27f29807b6e1c6645b10553e67d8d5
Tomasz Weglinski <tomasz.weglinski@yougov.com>
2026-02-01 17:19:57 +0100

    GHA PR fix;

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
+  reporter: [["html", { outputFolder: "playwright-report" }]],
   use: {
     baseURL: "http://localhost:3000",
     trace: "on-first-retry",
